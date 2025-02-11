79a63f830c129655f85e66b48085cf873d255345
noahBarreau <nonoferrari04@gmail.com>
2025-02-11 15:12:38 +0100

    toutes les interfaces, checkpoint

--- Assets/Scripts/BallAction.cs
+++ Assets/Scripts/BallAction.cs
@@ -1,15 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class BallRespawn : MonoBehaviour
+public class BallAction : MonoBehaviour
 {
     public Vector3 respawnPoint = Vector3.zero;
     public PopupManager popupManager;
+    public Button startButton;
+    public TMP_Text timerText;
+    public GameObject chrono;
+    public GameObject spawn;
+    public GameObject pausePanel;
+
+    public float elapsedTime = 0f;
+    public bool isTimerRunning = false;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             Respawn();
+        }
+
+        if (isTimerRunning)
+        {
+            elapsedTime += Time.deltaTime;
+            UpdateTimerText();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 
@@ -22,13 +49,41 @@ public class BallRespawn : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Finish"))
         {
-            popupManager.ShowPopup("Bravo, tu as marqué + que le jeu");
+            popupManager.ShowPopup("Bravo, tu as marquï¿½ + que le jeu");
+            StopTimer();
         }
+    }
+
+    public void StartGame()
+    {
+        isTimerRunning = true;
+        elapsedTime = 0f;
+        chrono.SetActive(true);
+    }
+
+    public void StopTimer()
+    {
+        isTimerRunning = false;
+    }
+
+    public void UpdateTimerText()
+    {
+        int hours = Mathf.FloorToInt(elapsedTime / 3600);
+        int minutes = Mathf.FloorToInt((elapsedTime % 3600) / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
     }
 
     void Respawn()
     {
         GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
-        transform.localPosition = respawnPoint;
+        transform.position = spawn.GetComponent<Transform>().position;
+    }
+
+    public void ClosePause()
+    {
+        pausePanel.SetActive(false);
+
+        Time.timeScale = 1f;
     }
 }
